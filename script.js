f4fa2e13ac595fc9135a13b6c7c3479e46d6e35d
Jenniferwonder <amazing_coder@163.com>
2022-10-14 18:27:27 +0800

    Add tabbed component

--- script.js
+++ script.js
@@ -33,7 +33,7 @@ document.addEventListener('keydown', function (e) {
 });
 
 ///////////////////////////////////////
-// *Button smooth scrolling
+// *Button Smooth Scrolling
 
 const btnScrollTo = document.querySelector('.btn--scroll-to');
 const section1 = document.querySelector('#section--1');
@@ -69,7 +69,7 @@ btnScrollTo.addEventListener('click', function (e) {
 });
 
 ///////////////////////////////////////
-// *Navigation smooth scrolling
+// *Navigation Smooth Scrolling
 /* document.querySelectorAll('.nav__link').forEach(function (el) {
   el.addEventListener('click', function (e) {
     e.preventDefault();
@@ -90,4 +90,30 @@ document.querySelector('.nav__links').addEventListener('click', function (e) {
     const id = e.target.getAttribute('href');
     document.querySelector(id).scrollIntoView({ behavior: 'smooth' });
   }
+});
+
+///////////////////////////////////////
+// *Tabbed Component
+
+const tabs = document.querySelectorAll('.operations__tab');
+const tabsContainer = document.querySelector('.operations__tab-container');
+const tabsContent = document.querySelectorAll('.operations__content');
+
+tabsContainer.addEventListener('click', function (e) {
+  const clicked = e.target.closest('.operations__tab');
+
+  // Guard clause
+  if (!clicked) return;
+
+  // Remove active classes
+  tabs.forEach(t => t.classList.remove('operations__tab--active'));
+  tabsContent.forEach(c => c.classList.remove('operations__content--active'));
+
+  // Activate tab
+  clicked.classList.add('operations__tab--active');
+
+  // Activate content area
+  document
+    .querySelector(`.operations__content--${clicked.dataset.tab}`)
+    .classList.add('operations__content--active');
 });
