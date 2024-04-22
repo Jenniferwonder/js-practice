d37eb0921d3b27308c0685104769205d1f2bf2b7
Jenniferwonder <Jenniferwonder@163.com>
2024-04-22 18:57:02 +0800

    chore: review

---
+++ array/array.js
@@ -0,0 +1,14 @@
+// let arr = ["foo", "bar"];
+// let newArr = arr.concat("baz");
+// let test = arr.splice(1, 0, "mor");
+// console.log(newArr);
+// console.log(test);
+// console.log(arr);
+
+let str = "hellor, world";
+let arr = ["hello", " world"];
+let newStr = str.split(",");
+// let newStr = arr.join(",");
+console.log(newStr);
+// let matches = str.match(/or/);
+// console.log(matches[0]);
--- data-fetch/debounce-throttle/practice/main.js
+++ data-fetch/debounce-throttle/practice/main.js
@@ -14,7 +14,121 @@ inputText.addEventListener("input", (e) => {
 	updateDebounceText(e.target.value);
 	updateThrottleText(e.target.value);
 });
+// Rv4-20240418 Good
 function debounce(cb, delay = 1000) {
+	let timeout;
+	return (...args) => {
+		clearTimeout(timeout);
+		timeout = setTimeout(() => {
+			cb(...args);
+		}, delay);
+	};
+}
+function throttle(cb, delay = 1000) {
+	let shouldWait = false;
+	let waitingArgs;
+	const timeoutFunc = () => {
+		cb(...waitingArgs);
+		setTimeout(timeoutFunc, delay);
+	};
+	return (...args) => {
+		if (shouldWait) {
+			waitingArgs = args;
+		}
+		cb(...args);
+		shouldWait = true;
+		setTimeout(timeoutFunc, delay);
+	};
+}
+// Rv3-20240417 Good
+// function debounce(cb, delay = 1000) {
+// 	let timeout;
+// 	return (...args) => {
+// 		clearTimeout(timeout);
+// 		timeout = setTimeout(() => {
+// 			cb(...args);
+// 		}, delay);
+// 	};
+// }
+// function throttle(cb, delay = 1000) {
+// 	let shouldWait = false;
+// 	let waitingArgs;
+// 	const timeoutFunc = () => {
+// 		cb(...waitingArgs);
+// 		setTimeout(timeoutFunc, delay);
+// 	};
+// 	return (...args) => {
+// 		if (shouldWait) {
+// 			waitingArgs = args;
+// 		}
+// 		cb(...args);
+// 		shouldWait = true;
+// 		setTimeout(timeoutFunc, delay);
+// 	};
+// }
+
+// Rv2-20240416 Hard
+// function debounce(cb, delay = 1000) {
+// 	let timeout;
+// 	return (...args) => {
+// 		clearTimeout(timeout);
+// 		timeout = setTimeout(() => {
+// 			cb(...args);
+// 		}, delay);
+// 	};
+// }
+// function throttle(cb, delay = 1000) {
+// 	let shouldWait = false;
+// 	let waitingArgs;
+// 	const timeoutFunc = () => {
+// 		cb(...waitingArgs);
+// 		setTimeout(timeoutFunc, delay);
+// 	};
+// 	return (...args) => {
+// 		if (shouldWait) {
+// 			waitingArgs = args;
+// 			return;
+// 		}
+// 		cb(...args);
+// 		shouldWait = true;
+// 		setTimeout(timeoutFunc, delay);
+// 	};
+// }
+
+// Rv1-20240409
+// function debounce(cb, delay = 1000) {
+// 	let timeout;
+// 	return (...args) => {
+// 		clearTimeout(timeout); // 时间不到再次触发须重新计时
+// 		timeout = setTimeout(() => {
+// 			cb(...args);
+// 		}, delay);
+// 	};
+// }
+// function throttle(cb, delay = 1000) {
+// 	let shouldWait = false;
+// 	let waitingArgs;
+// 	const timeoutFunc = () => {
+// 		// if (waitingArgs == null) {
+// 		// 	shouldWait = false;
+// 		// } else {
+// 		cb(...waitingArgs);
+// 		// waitingArgs = null;
+// 		setTimeout(timeoutFunc, delay);
+// 		// }
+// 	};
+// 	return (...args) => {
+// 		if (shouldWait) {
+// 			waitingArgs = args;
+// 			return;
+// 		}
+// 		cb(...args);
+// 		shouldWait = true;
+// 		setTimeout(timeoutFunc, delay);
+// 	};
+// }
+
+/* function debounce(cb, delay = 1000) {
 	let timeout;
 	return (...args) => {
 		clearTimeout(timeout);
@@ -39,4 +153,4 @@ function throttle(cb, delay = 1000) {
 		shouldWait = true;
 		setTimeout(timeoutFunc, delay);
 	};
-}
+} */
